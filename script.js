bcd42a7cd28dceea8d98202797965d3c4bc26c8d
AlliSwyt <swytap22@wfu.edu>
2026-03-03 21:08:28 -0500

    Just thought I'd mention this but I promise the bright yellow hitboxes won't be there forever, I'm just using them for testing; also I added more images and a key collection !

--- script.js
+++ script.js
@@ -10,11 +10,22 @@ const backArrow = document.getElementById('master-back-arrow');
 
 const bookDropDoorZoomOut = document.getElementById('bookDropDoorZoomOutPage');
 const bookDropDoorZoomIn = document.getElementById('bookDropDoorZoomInPage')
-const bookDropDoorHitbox = document.getElementById('bookDropDoor-hitbox');
-const bookDrop = document.getElementById('bookDropClosedPage');
-const bookDropHitbox = document.getElementById('bookDrop-hitbox');
-const bookDropDoorHandleHitbox = document.getElementById("bookDropDoorHandle-hitbox");
 const bookDropDoorHandle = document.getElementById("bookDropDoorHandlePage");
+const bookDropClosed = document.getElementById('bookDropClosedPage');
+const bookDropOpenKey = document.getElementById("bookDropOpenKeyPage");
+const bookDropOpen = document.getElementById("bookDropOpenPage");
+
+//booleans to track whether user has keys
+let hasBookDropKey = false;
+
+//hitboxes
+const bookDropHitbox = document.getElementById('bookDrop-hitbox');
+const bookDropDoorHitbox = document.getElementById('bookDropDoor-hitbox');
+const bookDropDoorHandleHitbox = document.getElementById("bookDropDoorHandle-hitbox");
+const bookDropClosedHitbox = document.getElementById("bookDropClosed-hitbox");
+const bookDropOpenHitbox = document.getElementById('bookDropOpen-hitbox');
+const bookDropKeyHitbox = document.getElementById("bookDropKey-hitbox");
+
 
 
 
@@ -26,25 +37,23 @@ function playTutorial() {
     //FIXME add text boxes for tutorial
 
     //hid menu, show tutorial first page
-    menu.classList.add('hidden');
     tutorial.classList.remove('hidden');
-    bookDropDoorZoomOut.classList.remove('hidden');
-    backArrow.classList.remove('hidden'); //fixme currently no functionality, want it to back up to view the hallway later ? or maybe remove for this image
+    goToBookDropDoorZoomOut();
+    backArrow.classList.remove('hidden');
 
     //if they click on the door, it zooms in on it
     bookDropDoorHitbox.addEventListener('click', goToBookDropDoorZoomIn);
     //backArrow.addEventListener('click', goTo...);
 
     function goToBookDropDoorZoomIn() {
-
         //hide book drop zoomed out image
         hideAllTutorialPages();
 
         //show book drop zoomed in image
         bookDropDoorZoomIn.classList.remove('hidden');
-        backArrow.addEventListener('click', goToBookDropDoorZoomOut);
+        backArrow.addEventListener('click', goBack);
 
-        bookDropHitbox.addEventListener('click', goToBookDrop);
+        bookDropHitbox.addEventListener('click', goToBookDropClosed);
         bookDropDoorHandleHitbox.addEventListener('click', goToBookDropDoorHandle);
 
     }
@@ -52,25 +61,63 @@ function playTutorial() {
     function goToBookDropDoorZoomOut() {
         hideAllTutorialPages();
         bookDropDoorZoomOut.classList.remove('hidden');
-        bookDropHitbox.addEventListener('click', goToBookDrop);
-        bookDropDoorHandleHitbox.addEventListener('click', goToBookDropDoorHandle);
-
+        bookDropDoorHitbox.addEventListener('click', goToBookDropDoorZoomIn);
     }
-    function goToBookDrop() {
+    function goToBookDropClosed() {
         hideAllTutorialPages();
         bookDropDoorZoomIn.classList.add('hidden');
-        bookDrop.classList.remove('hidden');
+        bookDropClosed.classList.remove('hidden');
+
+        bookDropClosedHitbox.addEventListener('click', goToBookDropOpen);
+
+        backArrow.addEventListener('click', goBack);
     }
 
     function goToBookDropDoorHandle() {
         hideAllTutorialPages();
-        bookDropDoorZoomIn.classList.add('hidden');
         bookDropDoorHandle.classList.remove('hidden');
+        backArrow.addEventListener('click', goBack);
+    }
+
+    function goToBookDropOpen() {
+        hideAllTutorialPages();
+        backArrow.addEventListener('click', goBack)
+        if (!hasBookDropKey) {
+            bookDropOpenKey.classList.remove('hidden');
+            bookDropKeyHitbox.addEventListener('click', collectBookDropKey)
+        }
+        else {
+            bookDropOpen.classList.remove('hidden');
+            bookDropOpenHitbox.addEventListener('click', goToBookDropClosed);
+        }
+    }
+
+    function collectBookDropKey() {
+        hasBookDropKey = true;
+        goToBookDropOpen();
     }
 
     function hideAllTutorialPages() {
         const pages = document.querySelectorAll('.fit');
         pages.forEach(page => page.classList.add('hidden'));
+    }
+
+
+    //Fixme add backArrow check which page functionality bc of bug with the backArrow rn
+
+    function goBack() {
+        if (!bookDropDoorZoomIn.classList.contains('hidden')) {
+            goToBookDropDoorZoomOut();
+        }
+        else if (!bookDropClosed.classList.contains('hidden')) {
+            goToBookDropDoorZoomIn();
+        }
+        else if (!bookDropOpen.classList.contains('hidden') || !bookDropOpenKey.classList.contains('hidden')) {
+            goToBookDropClosed();
+        }
+        else if (!bookDropDoorHandle.classList.contains('hidden')) {
+            goToBookDropDoorZoomIn();
+        }
     }
 
 }
