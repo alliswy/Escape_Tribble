332a39e01270e6dedcd67f2eb9cc133ad5a9e703
AlliSwyt <alli.swyt@gmail.com>
2026-03-25 15:20:28 -0400

    I re-added my hitboxes again

--- script.js
+++ script.js
@@ -43,11 +43,11 @@ function goBack() {
         case 'bd-door-page':
         case 'bd-door-open-page':   showPage('bd-main-page'); break;
 
-        case 'bd-closed-page':
+        case 'bd-slot-closed-page':
         case 'bd-door-handle-page': showPage('bd-door-page'); break;
 
         case 'bd-slot-open-key-page':
-        case 'bd-slot-open-page':   showPage('bd-closed-page'); break;
+        case 'bd-slot-open-page':   showPage('bd-slot-closed-page'); break;
 
         case 'bd-cart-page':
         case 'bd-back-door-handle-page':    showPage('bd-door-open-page'); break;
@@ -208,7 +208,7 @@ function init() {
     document.getElementById('bd-door-hitbox').onclick = () => showPage('bd-door-page');
 
     // Door -> Slot or Handle
-    document.getElementById('bd-slot-hitbox').onclick = () => showPage('bd-closed-page');
+    document.getElementById('bd-slot-hitbox').onclick = () => showPage('bd-slot-closed-page');
     document.getElementById('bd-handle-hitbox').onclick = () => showPage('bd-door-handle-page');
 
     // Inside the Slot
@@ -223,6 +223,7 @@ function init() {
         }
         showPage('bd-slot-open-page');
     };
+    document.getElementById('bd-slot-open-hitbox').onclick = () => showPage('bd-slot-closed-page');
 
     // bd Door Handle & Locking
     document.getElementById('bd-door-keyhole-hitbox').onclick = () => {
