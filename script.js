d58f3d83599308fe1790c3620a816ad2cc224506
Your Personal Name <alli.swyt@gmail.com>
2026-04-01 00:47:10 -0400

    fixing some comments

--- script.js
+++ script.js
@@ -214,7 +214,7 @@ const roomLeads = {
 
     //c-wing side halls
     'print-page':               {back: 'cw-right-print-page'},
-    'print-screen-page':        {back: 'print-page'},
+    'print-screen-page':        {back: 'print-page'}, //fixme add more for this page
     'cw-eh-entrance-page':      {forward: 'eh-door-page', right: 'cw-right-eh-page'}, //fixme add left
     'eh-door-page':             {back: 'cw-eh-entrance-page'},
     'eh-door-plate-page':       {back: 'eh-door-page'},
